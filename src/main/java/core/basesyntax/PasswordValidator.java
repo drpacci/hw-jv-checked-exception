be36019ffb8bfa996052c3c2ad1bddb9628555dc
Denys <werdumbox@gmail.com>
2026-06-28 22:38:29 +0200

    check on null added and fixed throw issue

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
 public class PasswordValidator {
+
     void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        int maxLength = 10;
-        if (password.length() < maxLength || !password.equals(repeatPassword)) {
-            throw new IllegalArgumentException("Wrong passwords");
+        int minLength = 10;
+
+        if (password == null || repeatPassword == null) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
+
+        if (password.length() < minLength || !password.equals(repeatPassword)) {
+            throw new PasswordValidationException("Wrong passwords");
         }
     }
 }
