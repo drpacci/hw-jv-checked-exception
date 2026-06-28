79c0ccdd4339451777ac56a268ef604279d70546
Denys <werdumbox@gmail.com>
2026-06-28 22:31:53 +0200

    fixed checkstyle issues

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,7 +1,7 @@
 package core.basesyntax;
 
 public class PasswordValidator {
-    void validate (String password, String repeatPassword)
+    void validate(String password, String repeatPassword)
             throws PasswordValidationException {
         int maxLength = 10;
         if (password.length() < maxLength || !password.equals(repeatPassword)) {
